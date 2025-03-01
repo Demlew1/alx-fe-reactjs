360f9caffdcdb507ab59941b819ac13188ef9818
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-01 16:11:32 +0300

    finished building useRecipeForm store and created 2 components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
