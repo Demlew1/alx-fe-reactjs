7a43b1034eb169605845987d3647ae0fe1991068
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-09 14:45:29 +0300

    updated some codes

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,16 +1,13 @@
-import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/formikForm";
+import RegistrationForm from "./components/RegistrationForm";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>User Registration (Controlled Form)</h1>
+    <>
       <RegistrationForm />
-
-      <h1>User Registration (Formik Form)</h1>
       <FormikForm />
-    </div>
+    </>
   );
-};
+}
 
 export default App;
