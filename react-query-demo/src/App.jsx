a20c344cd50967d4eaf7ba0f20835d6cf9d3bbbb
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-13 13:05:14 +0300

    changed some stuff for my checker

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,14 @@
 import PostsComponent from "./components/PostsComponent";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const client = new QueryClient();
 function App() {
-  return <PostsComponent />;
+  return (
+    <QueryClientProvider client={client}>
+      <PostsComponent />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
