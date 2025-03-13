6fd2b735cead8fdaff8ed330c5652a1a7bf1edd0
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-13 12:57:59 +0300

    edited some codes for the checker

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,16 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PostsComponent from "./components/PostsComponent";
 
-const client = new QueryClient();
-
 function App() {
-  return (
-    <QueryClientProvider client={client}>
-      <PostsComponent />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <PostsComponent />;
 }
 
 export default App;
--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+const client = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
