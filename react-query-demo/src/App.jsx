88687720829727862ad369789d05784cafc2214c
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-13 13:28:01 +0300

    still adjusting the code

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react";
 import PostsComponent from "./components/PostsComponent";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-const client = new QueryClient();
+
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
