df8468e2b6c47139bd953e53635f951344822ad7
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-13 14:27:25 +0300

    finished the whole project

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,6 +1,5 @@
-import RegistrationForm from "./components/RegistrationForm.jsx";
-import formikForm from "./components/formikForm.js"
 import { useState } from 'react'
+import TodoList from './components/TodoList.jsx'; // Importing TodoList component
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +9,11 @@ function App() {
 
   return (
     <>
+    return (
+      <div className="App">
+        <TodoList /> {/* Rendering TodoList */}
+      </div>
+    )
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,14 +23,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-
-      <div>
-        <h2>Controlled Form</h2>
-        <RegistrationForm />
-        <h2>Formik Form</h2>
-        <formikForm />
-      </div>
-
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
