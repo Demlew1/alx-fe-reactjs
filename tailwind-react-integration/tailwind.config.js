0ff4623925de925d32ef18906184c75f0b436291
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-03-15 10:02:32 +0300

    satisfying

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // replaces 'purge' in v3+
+  darkMode: "class", // 'media' (default) or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
