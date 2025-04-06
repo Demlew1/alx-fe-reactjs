24aa0ca54ed707e5f0544f89ea2438f11cb75e77
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-04-06 11:18:30 +0300

    finished task 1

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start searching for users above 👆</p>
+    </div>
+  );
+}
+
+export default App;
